ca8341cdecda7cc57c91f23a4f889c9939b1f765
Shahadat-BD <shahadatstudent34@gmail.com>
2023-09-23 23:14:49 +0000

    project setup complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <h1 className='text-3xl font-bold text-manrope text-center mt-10'>This is world jobs finder website.</h1>
+    </>
+  )
+}
+
+export default App
